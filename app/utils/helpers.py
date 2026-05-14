a9132db8725367839eb96880f10618a1b0897520
Anusha Chalageri <chalagerianusha4@gmail.com>
2026-05-14 08:07:44 +0530

    fix: ensure time objects are JSON serializable

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -32,18 +32,25 @@ DAY_NAMES = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
 
 def get_user_availability(user_id):
     from app.models import Availability
+    from datetime import time
     availabilities = Availability.query.filter_by(user_id=user_id).all()
-    return [{
-        'day': DAY_NAMES[a.day_of_week],
-        'day_of_week': a.day_of_week,
-        'start': a.start_time,
-        'end': a.end_time,
-        'start_str': a.start_time.strftime('%H:%M'),
-        'end_str': a.end_time.strftime('%H:%M')
-    } for a in availabilities]
+    result = []
+    for a in availabilities:
+        start = a.start_time if isinstance(a.start_time, time) else a.start_time
+        end = a.end_time if isinstance(a.end_time, time) else a.end_time
+        result.append({
+            'day': DAY_NAMES[a.day_of_week],
+            'day_of_week': a.day_of_week,
+            'start': start.strftime('%H:%M'),
+            'end': end.strftime('%H:%M'),
+            'start_str': start.strftime('%H:%M'),
+            'end_str': end.strftime('%H:%M')
+        })
+    return result
 
 
 def get_common_availability(user1, user2):
+    from datetime import time
     user1_avail = get_user_availability(user1.id)
     user2_avail = get_user_availability(user2.id)
     
@@ -51,14 +58,18 @@ def get_common_availability(user1, user2):
     for slot1 in user1_avail:
         for slot2 in user2_avail:
             if slot1['day_of_week'] == slot2['day_of_week']:
-                start = max(slot1['start'], slot2['start'])
-                end = min(slot1['end'], slot2['end'])
+                start1 = time(int(slot1['start'].split(':')[0]), int(slot1['start'].split(':')[1]))
+                end1 = time(int(slot1['end'].split(':')[0]), int(slot1['end'].split(':')[1]))
+                start2 = time(int(slot2['start'].split(':')[0]), int(slot2['start'].split(':')[1]))
+                end2 = time(int(slot2['end'].split(':')[0]), int(slot2['end'].split(':')[1]))
+                start = max(start1, start2)
+                end = min(end1, end2)
                 if start < end:
                     common.append({
                         'day': slot1['day'],
                         'day_of_week': slot1['day_of_week'],
-                        'start': start,
-                        'end': end,
+                        'start': start.strftime('%H:%M'),
+                        'end': end.strftime('%H:%M'),
                         'start_str': start.strftime('%H:%M'),
                         'end_str': end.strftime('%H:%M')
                     })
